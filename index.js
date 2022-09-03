70056e082a058c1d0b3bed933a6c52166bc780fa
hernays <hernays12@gmail.com>
2022-09-03 18:07:49 -0400

    fix:

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-/* import  { Server }  from "./modelos/server.js";
+ import  { Server }  from "./modelos/server.js";
 
 
 const server = new Server();
 
-server.iniciar(); */
+server.iniciar(); 
 
-import express from 'express';
-
-const app = express();
-
-app.get('/' , (req , res) => {
-    console.log('alguien entro')
-    res.send('hola mundo')
-})
-app.listen( 4000, () => {
-
-    console.log('servidor ok')
-})
