8bb1936942148aa815ad29875f0f079e04f47f17
hernays <hernays12@gmail.com>
2022-09-03 18:00:35 -0400

    fix

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/' , (req , res) => {res.send('hola mundo')})
+app.get('/' , (req , res) => {
+    console.log('alguien entro')
+    res.send('hola mundo')
+})
 app.listen( 4000, () => {
 
     console.log('servidor ok')
