150634775d6e1c809f19a599050343d6a324acce
hernays <hernays12@gmail.com>
2022-09-03 17:38:38 -0400

    fix: dfdd

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import  { Server }  from "./modelos/server.js";
+/* import  { Server }  from "./modelos/server.js";
 
 
 const server = new Server();
 
-server.iniciar();
+server.iniciar(); */
+
+import express from 'express';
+
+const app = express();
+
+app.get('/' , (req , res) => {res.send('hola mundo')})
+app.listen( 4000, () => {
+
+    console.log('servidor ok')
+})
